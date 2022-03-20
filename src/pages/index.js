c45f28be170881016e8c160b280f20fd0d2b5ab3
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-03-20 12:25:00 +0700

    change title

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Pandawa::${siteConfig.title}`}
+      title={siteConfig.title}
       description="Just a documentation">
       <HomepageHeader />
       <main>
