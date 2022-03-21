e30f4ac8e22dfcc11c3db394cf2cace4cc61a811
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-03-21 18:45:15 +0700

    use docusaurus link elem

--- src/components/Markdown/readMore/index.js
+++ src/components/Markdown/readMore/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Admonition from '@theme/Admonition';
+import Link from '@docusaurus/Link';
 
 export default function Example({ url, label }) {
   return (
     <Admonition type="tip">
-      Read more about <a href={url}>{label}</a>.
+      Read more about <Link to={url}>{label}</Link>.
     </Admonition>
   );
 }
