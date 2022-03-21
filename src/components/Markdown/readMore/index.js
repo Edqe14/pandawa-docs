9b0505fed1d7f2b8bb04748164ec071345d45adf
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-03-21 19:09:08 +0700

    add read more component

--- src/components/Markdown/readMore/index.js
+++ src/components/Markdown/readMore/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Admonition from '@theme/Admonition';
 import Link from '@docusaurus/Link';
 
-export default function Example({ url, label }) {
+export default function Example({ url, label, inline }) {
+  if (inline) {
+    return (
+      <i>Read more about <strong>{label}</strong> <Link to={url}>here</Link></i>
+    )
+  }
+
   return (
     <Admonition type="tip">
       Read more about <Link to={url}>{label}</Link>.
