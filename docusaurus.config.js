dcb73ec4aa1ec2e1795326a029c6170a2896ae84
Edqe <34704796+Edqe14@users.noreply.github.com>
2022-03-22 14:38:55 +0700

    fix title & add pwa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 const config = {
   title: 'Pandawa',
   tagline: 'An Extensive and Modular Laravel Framework',
-  url: 'https://pandawa.com',
+  url: 'https://edqe14.github.io',
   baseUrl: '/pandawa-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -44,6 +44,9 @@ const config = {
     ({
       navbar: {
         title: 'Pandawa',
+        logo: {
+          src: '/img/logo.png'
+        },
         items: [
           {
             type: 'doc',
@@ -100,7 +103,55 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['php']
       },
+      image: '/img/banner.png'
     }),
+
+  plugins:
+    ([
+      [
+        '@docusaurus/plugin-pwa',
+        {
+          debug: true,
+          offlineModeActivationStrategies: [
+            'appInstalled',
+            'standalone',
+            'saveData',
+            'queryString'
+          ],
+          pwaHead: [
+            {
+              tagName: 'link',
+              rel: 'icon',
+              href: '/img/logo.png',
+            },
+            {
+              tagName: 'link',
+              rel: 'manifest',
+              href: '/manifest.json', // your PWA manifest
+            },
+            {
+              tagName: 'meta',
+              name: 'theme-color',
+              content: '#7D50E5',
+            },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '57x57', href: '/img/icons/apple-icon-57x57.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '57x57', href: '/img/icons/apple-icon-57x57.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '60x60', href: '/img/icons/apple-icon-60x60.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '72x72', href: '/img/icons/apple-icon-72x72.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '76x76', href: '/img/icons/apple-icon-76x76.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '114x114', href: '/img/icons/apple-icon-114x114.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '120x120', href: '/img/icons/apple-icon-120x120.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '144x144', href: '/img/icons/apple-icon-144x144.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '152x152', href: '/img/icons/apple-icon-152x152.png' },
+            { tagName: 'link', rel: 'apple-touch-icon', sizes: '180x180', href: '/img/icons/apple-icon-180x180.png' },
+            { tagName: 'link', rel: 'icon', sizes: '192x192', href: '/img/icons/android-icon-192x192.png' },
+            { tagName: 'link', rel: 'icon', sizes: '32x32', href: '/img/icons/favicon-32x32.png' },
+            { tagName: 'link', rel: 'icon', sizes: '96x96', href: '/img/icons/favicon-96x96.png' },
+            { tagName: 'link', rel: 'icon', sizes: '16x16', href: '/img/icons/favicon-16x16.png' }
+          ],
+        }
+      ]
+    ])
 };
 
 module.exports = config;
